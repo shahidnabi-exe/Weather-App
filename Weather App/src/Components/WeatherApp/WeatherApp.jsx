57abb16835515bd9d7f118fe11601a0101e8cd90
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-05-15 03:43:33 +0500

    second commit

---
+++ Weather App/src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './WeatherApp.css'
+import searchIcon from '../Assets/search.png'
+import clearIcon from '../Assets/clear.png'
+import rainIcon from '../Assets/rain.png'
+import snowIcon from '../Assets/snow.png'
+import windIcon from '../Assets/wind.png'
+import drizzleIcon from '../Assets/drizzle.png'
+import cloudIcon from '../Assets/cloud.png'
+import humidityIcon from '../Assets/humidity.png'
+
+
+
+
+
+function WeatherApp() {
+  return (
+    <div>
+        WeatherApp
+    </div>
+  )
+}
+
+export default WeatherApp
