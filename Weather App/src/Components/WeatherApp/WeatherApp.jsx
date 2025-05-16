846931e6c3fa5ba44172742d73a0701b12c3d149
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-05-16 19:35:22 +0500

    third commit

--- Weather App/src/Components/WeatherApp/WeatherApp.jsx
+++ Weather App/src/Components/WeatherApp/WeatherApp.jsx
@@ -10,14 +10,53 @@ import cloudIcon from '../Assets/cloud.png'
 import humidityIcon from '../Assets/humidity.png'
 
 
-
-
-
 function WeatherApp() {
+
+  const Weather = () => {
+  let APIkey =   "a2f00fdfa3b2821e0eef761440d98bca";
+
+  const search = () => {
+    const element = document.getElementsByClassName("cityInput");
+      
+  }
+
+  }
+
   return (
-    <div>
-        WeatherApp
+    <div className='container'>
+      <div className="top-bar">
+        <input type="text" className='cityInput' placeholder='Search City' />
+        <div className="search-icon">
+          <img src={searchIcon} alt="Search" />
+        </div>
+      </div>        
+
+      <div className="weather-img">
+        <img src={cloudIcon} alt="" />
+      </div>
+      <div className="weather-temp">24°c </div>
+      <div className="weather-location">Islamabad</div>
+
+      <div className="data-container">
+        <div className="element">
+          <img src={humidityIcon} alt=""  className='icon'/>
+          <div className="data">
+            <div className="humidity-percent">60%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+    
+        <div className="element">
+          <img src={windIcon} alt="" className='icon' />
+          <div className="data">
+            <div className="Wind-percent">60%</div>
+            <div className="text">Wind</div>
+          </div>
+         
+        </div>
+      </div>
     </div>
+
   )
 }
 
